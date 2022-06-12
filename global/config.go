43db4e8bfabc2f4c09f7a4b6ca81f8c440f73b1e
StuGeek <978475534@qq.com>
2022-06-12 13:48:16 +0800

    add max num

--- global/config.go
+++ global/config.go
@@ -7,5 +7,5 @@ var SqlPassword = "123456"               // 数据库的密码
 var SqlDBName = "simple_tiktok"          // 使用的数据库名
 var SqlDemoDBName = "demo_simple_tiktok" // 导入demo数据使用的数据库名
 
-var MaxFollowUserCount int64 = 10000 // 单个用户最多关注的用户数
-var MaxCommentCount int64 = 1000000  // 单个视频评论的最大数
+var MaxFollowUserCount int64 = 100000000 // 单个用户最多关注的用户数
+var MaxCommentCount int64 = 100000000000 // 单个视频评论的最大数
